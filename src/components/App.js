3a4c246afb99cc063046713d6933ca95084d7a3f
John Komba <john.mwangi1@student.moringaschool.com>
2023-10-27 14:18:46 +0300

    done with assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImageUrl, setDogImageUrl] = useState(null);
+
+  useEffect(() => {
+    // Send a fetch request to get a random dog image
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        setDogImageUrl(data.message);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div>
+      {dogImageUrl ? (
+        <img src={dogImageUrl} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
